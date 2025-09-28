3a8a520546cafa1f8cf8ff03582e470310dba408
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 22:21:19 +0300

    إضافة خوارزمية Base64 (تشفير/فك)

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -51,7 +51,7 @@ class AboutPage extends StatelessWidget {
                 const SizedBox(height: 8),
                 const Text(
                   '   طرق التشفير المختلف( Base64، Caesar) '
-                      'مع واجهة    وإعدادات تخصيص.',
+                      'مع     وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
                 Wrap(
