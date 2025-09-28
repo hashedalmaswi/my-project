9dd0de3fa3640e91ce1ff201e5da7a93656691c2
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 22:22:13 +0300

    إإضافة خوارزمية AES (تشفير/فك)

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
                 Text('SecureApp', style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: 8),
                 const Text(
-                  '   طرق التشفير المختلف( Base64، Caesar) '
+                  '    التشفير المختلف( Base64، Caesar) '
                       '     وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
