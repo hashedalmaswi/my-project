c88e21c38f1c2793b7fbc6e8c3405a198c6f1885
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 23:28:21 +0300

    إضافة أزرار عمليات التشفير

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
                 Text('SecureApp', style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: 8),
                 const Text(
-                  '     المختلفة (AES، Base64، Caesar) '
+                  '     المختلفة (AES،  Caesar) '
                       'مع واجهة   عمليات وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
