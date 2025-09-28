f9b40fc1de1d1c5676518b7c22c5e111ac3eb6fb
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 16:15:11 +0300

    إضافة ملف main.dart مع تطبيق بسيط

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
                 Text('SecureApp', style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: 8),
                 const Text(
-                  'تطبيق تعليمي متقدم يوضح طرق التشفير المختلفة (AES، Base64، Caesar) '
+                  'تطبيقعليمي متقدم يوضح طرق التشفير المختلفة (AES، Base64، Caesar) '
                       'مع واجهة أنيقة وسجل عمليات وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
