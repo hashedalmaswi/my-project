3fdf297fe5ee30ea27dc22490ba5c244f0ca4430
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 23:27:38 +0300

    إضافة تنسيقات TextField

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
                 Text('SecureApp', style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: 8),
                 const Text(
-                  '    التشفير المختلفة (AES، Base64، Caesar) '
+                  '     المختلفة (AES، Base64، Caesar) '
                       'مع واجهة   عمليات وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
