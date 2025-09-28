89cb8cad89683292332d996a3af0398067981f8a
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 22:21:51 +0300

    إضافة خوارزمية Caesar (تشفير/فك)

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -51,7 +51,7 @@ class AboutPage extends StatelessWidget {
                 const SizedBox(height: 8),
                 const Text(
                   '   طرق التشفير المختلف( Base64، Caesar) '
-                      'مع     وإعدادات تخصيص.',
+                      '     وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
                 Wrap(
