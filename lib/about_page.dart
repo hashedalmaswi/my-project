3b7f72cca8abd4730bbb75c5383317084d3953b1
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 22:20:50 +0300

    إنشاء صفحة HistoryPage

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
                 Text('SecureApp', style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: 8),
                 const Text(
-                  '   طرق التشفير المختلف(AES، Base64، Caesar) '
+                  '   طرق التشفير المختلف( Base64، Caesar) '
                       'مع واجهة    وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
