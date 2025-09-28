cd84726b9fcca8e05178b6863245dd2934a73dd7
hashedalmaswi <almaswihashed@gmail.com>
2025-09-28 22:20:21 +0300

    إإضافة HistoryManager للتخزين

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -50,7 +50,7 @@ class AboutPage extends StatelessWidget {
                 Text('SecureApp', style: Theme.of(context).textTheme.headlineSmall),
                 const SizedBox(height: 8),
                 const Text(
-                  '  يوضح طرق التشفير المختلف(AES، Base64، Caesar) '
+                  '   طرق التشفير المختلف(AES، Base64، Caesar) '
                       'مع واجهة    وإعدادات تخصيص.',
                 ),
                 const SizedBox(height: 12),
